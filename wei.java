0bf64394d8969d374d6c131e0f4eec3272271429
narcissu <652944268@qq.com>
2025-09-13 15:22:48 +0800

    Add files via upload

---
+++ wei.java
@@ -0,0 +1,9 @@
+public class wei {
+    public static void main(String[] args) {
+        int a = -3;
+        int b = 3;
+        
+        System.out.println(b +  "往右俩个位是" + (b >> 2));
+        
+    }
+}
