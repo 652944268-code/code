481614fffb9228e9ebafee36241b4db31eed72a3
narcissu <652944268@qq.com>
2025-09-14 10:06:32 +0800

    Add files via upload

---
+++ solveQuadratic.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class solveQuadratic {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble(), b = sc.nextDouble(), c = sc.nextDouble();
+        double delta = b * b - 4 * a * c;
+        double x1 = (-b + Math.sqrt(delta)) / (2 * a);
+        double x2 = (-b - Math.sqrt(delta)) / (2 * a);
+        System.out.printf("%.4f\n%.4f", x1, x2);
+    }
+}
